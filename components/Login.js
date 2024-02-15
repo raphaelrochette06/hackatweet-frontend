20ca8206cb46820baf51f71b0a1a9b0fa144de5e
Kevin <kecosta1312@gmail.com>
2024-02-15 17:13:15 +0100

    connection back/front user

--- components/Login.js
+++ components/Login.js
@@ -2,100 +2,20 @@ import styles from "../styles/Login.module.css";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import Modal from "react-modal";
-
-// function Login() {
-//     const [modalIsOpen, setIsOpen] = useState(false);
-
-//     const customStyles = {
-//       content: {
-//         top: "20%",
-//         left: "50%",
-//         right: "auto",
-//         bottom: "auto",
-//         marginRight: "-50%",
-//         transform: "translate(-50%, -50%)",
-//       },
-//     };
-
-//     function openModal() {
-//       setIsOpen(true);
-//     }
-//     function closeModal() {
-//       setIsOpen(false);
-//     }
-
-// return(
-// <div>
-//     <div className = { styles.connection}>
-//         <div className= {styles.logoTwitter}>
-//             <Image src="/Logo_of_Twitter.svg.png" alt="logotwitter" width={600} height={314}/>
-
-//         </div>
-//         <div className={styles.connectionContainer}>
-//         <div className={styles.logoTwitterMini}>
-//             <Image src="/Logo_of_Twitter.svg.png" alt="logotwitter" width={100} height={100}/>
-//         </div>
-//            <div className={styles.titleConnection}>
-//             <h1 className={styles.title}>See what's happening</h1>
-//             <div className={styles.contentConnection}>
-//                 <h2 className={styles.joinText}> Join Hackatweet today.</h2>
-//                 <button onClick={openModal} className={styles.btnSignUp}> Sign up</button>
-//                 <Modal
-//           isOpen={modalIsOpen}
-//           onRequestClose={closeModal}
-//           style={customStyles}
-//           contentLabel="Example Modal"
-//         />
-//                 <p className={styles.text}> Already have an account?</p>
-//                 <button className={styles.btnSignIn}> Sign in</button>
-//             </div>
-//            </div>
-//         </div>
-//     </div>
-
-// </div>
-
-// )
-// }
-
-// // <button onClick={openModal}>Open Modal</button>
-// //         <Modal
-// //           isOpen={modalIsOpen}
-// //           onRequestClose={closeModal}
-// //           style={customStyles}
-// //           contentLabel="Example Modal"
-// //         >
-//   <button onClick={closeModal}>close</button>
-//   <div>Signup</div>
-//   <form>
-//     <input />
-//   </form>
-// //         </Modal>
-
-// export default Login
+import { login, logout } from "../reducers/user.js";
+import { useDispatch } from "react-redux";
 
 function Login() {
+  const dispatch = useDispatch();
   const [modalSignInIsOpen, setSignInIsOpen] = useState(false);
   const [modalSignUpIsOpen, setSignUpIsOpen] = useState(false);
 
-  const customStyles = {
-    content: {
-      top: "300px",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      height: "400px",
-      width: "550px",
-      borderRadius: "30px",
-      backgroundColor: "#161d28",
-      display: "flex",
-      justifyContent: "center",
-      flexDirection: "column",
-      alignItems: "center",
-    },
-  };
+  const [signUpFirstname, setSignUpFirstname] = useState("");
+  const [signUpUsername, setSignUpUsername] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
+
+  const [signInUsername, setSignInUsername] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
 
   function openModalSignUp() {
     setSignUpIsOpen(true);
@@ -107,13 +27,53 @@ function Login() {
 
   function openModalSignIn() {
     setSignInIsOpen(true);
-
     // Ajoutez ici votre logique spécifique pour le formulaire de connexion
   }
 
   function closeModalSignIn() {
     setSignInIsOpen(false);
   }
+
+  const handleRegister = () => {
+    fetch("http://localhost:3000/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firstname: signUpFirstname,
+        username: signUpUsername,
+        password: signUpPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          dispatch(login({ username: signUpUsername, token: data.token }));
+          setSignUpFirstname("");
+          setSignUpUsername("");
+          setSignUpPassword("");
+        }
+      });
+  };
+
+  const handleConnection = () => {
+    fetch("http://localhost:3000/users/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: signInUsername,
+        password: signInPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.result) {
+          dispatch(login({ username: signInUsername, token: data.token }));
+          setSignInUsername("");
+          setSignInPassword("");
+        }
+      });
+  };
 
   return (
     <div>
@@ -162,22 +122,33 @@ function Login() {
                     />
                     <h2> Create your Hackatweet account</h2>
 
-                    <form className={styles.signUpForm}>
+                    <div className={styles.signUpForm}>
                       <input
                         className={styles.firstname}
                         placeholder="Firstname"
+                        onChange={(e) => setSignUpFirstname(e.target.value)}
+                        value={signUpFirstname}
                       />
                       <input
                         className={styles.username}
                         placeholder="Username"
+                        onChange={(e) => setSignUpUsername(e.target.value)}
+                        value={signUpUsername}
                       />
                       <input
                         type="password"
                         className={styles.password}
                         placeholder="Password"
+                        onChange={(e) => setSignUpPassword(e.target.value)}
+                        value={signUpPassword}
                       />
-                    </form>
-                    <button className={styles.modalBtn}>Sign Up</button>
+                    </div>
+                    <button
+                      onClick={() => handleRegister()}
+                      className={styles.modalBtn}
+                    >
+                      Sign Up
+                    </button>
                   </div>
                 </div>
               </Modal>
@@ -198,18 +169,27 @@ function Login() {
                   </div>
                   <div className={styles.contentModal}>
                     <h2> Connect to Hackatweet</h2>
-                    <form className={styles.signInForm}>
+                    <div className={styles.signInForm}>
                       <input
                         className={styles.username}
                         placeholder="Username"
+                        onChange={(e) => setSignInUsername(e.target.value)}
+                        value={signInUsername}
                       />
                       <input
                         type="password"
                         className={styles.password}
                         placeholder="Password"
+                        onChange={(e) => setSignInPassword(e.target.value)}
+                        value={signInPassword}
                       />
-                    </form>
-                    <button className={styles.modalBtn}>Sign In</button>
+                    </div>
+                    <button
+                      onClick={() => handleConnection()}
+                      className={styles.modalBtn}
+                    >
+                      Sign In
+                    </button>
                   </div>
                 </div>
               </Modal>
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import user from "../reducers/user";
+import Login from "../components/Login";
+
+const store = configureStore({
+  reducer: { user },
+});
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
-        <title>Next.js App</title>
+        <title>Hackatweet</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
