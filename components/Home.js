86b51ff0cbca654c521b66dfc63ac6f9f02f84f0
Rochette Raphaël <rochette.raph@gmail.com>
2024-02-16 01:00:26 +0100

    create component Home + page

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,38 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 function Home() {
   return (
     <div>
       <div className={styles.homePage}>
-        <div className={styles.sideHeader}></div>
-        <div className={styles.mainContent}></div>
-        <div className={styles.trendContent}></div>
+        <div className={styles.sideHeader}>
+          <div className={styles.logoTwitterMini}>
+            <Image
+              src="/Logo_of_Twitter.svg.png"
+              alt="logotwitter"
+              width={50}
+              height={40}
+              className={styles.logoTwitter}
+            />
+          </div>
+          <div className={styles.profilInfo}>
+            <Image
+              src="/Egg-Twitter.png"
+              alt="ProfilePictureTwitter"
+              width={40}
+              height={40}
+              className={styles.profilePic}
+            />
+            <button className={styles.btnLogout}>Logout</button>
+          </div>
+        </div>
+        <div className={styles.mainContent}>
+          <h1 className={styles.title}>Home</h1>
+        </div>
+        <div className={styles.trendContent}>
+        <h2 className={styles.title}>Trends</h2>
+        </div>
       </div>
     </div>
   );
